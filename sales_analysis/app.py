e0ed75e7a5a330cd44a16524e7aecc7d63102240
Hwangyonghwan <iamyonghwan@nate.com>
2018-07-13 16:13:15 +0900

    add project folder

---
+++ sales_analysis/app.py
@@ -0,0 +1,131 @@
+#-*- coding: utf-8 -*-
+
+from __future__ import with_statement
+from contextlib import closing
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import time
+import crawling
+
+DATABASE = './crawling.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+
+@app.before_request
+def befor_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	g.db.close()
+
+@app.route('/index')
+def index():
+	sql = "SELECT A.UPDATE_DATE, SUM(A.STUDENT_COUNT * A.PRICE) AS TOTAL_REVENUE, SUM((A.STUDENT_COUNT - B.STUDENT_COUNT) * A.PRICE) AS DAILY_REVENUE, SUM((A.STUDENT_COUNT - B.STUDENT_COUNT) * A.PRICE) * 0.3 AS COMPANY_REVENUE FROM SALES A, SALES B WHERE A.ID = B.ID AND A.UPDATE_DATE = DATE(B.UPDATE_DATE, '+1 day') GROUP BY A.UPDATE_DATE"
+	cur = g.db.execute(sql)
+	index_table = [dict(DATE=row[0], TOTAL_REVENUE=format(int(row[1]), ','), DAILY_REVENUE=format(int(row[2]), ','), COMPANY_REVENUE=format(int(row[3]), ',')) for row in cur.fetchall()]
+
+	revenue_predict = 0
+	for index in index_table:
+		revenue_predict = revenue_predict + int(index['COMPANY_REVENUE'].replace(',',''))
+	revenue_predict = int( (revenue_predict/len(index_table) * 9 * 365) / (10 * 12) )
+
+	monthly_revenue_predict = format(revenue_predict, ',')
+
+	return render_template('index.html', INDEX_TABLE=index_table, MONTHLY_REVENUE_PREDICT=monthly_revenue_predict)
+
+@app.route('/daily/<date>')
+def daily_index(date):
+	sql = "SELECT COURSES.ID, COURSES.TITLE, A.PRICE, A.STUDENT_COUNT, SUM((A.STUDENT_COUNT - B.STUDENT_COUNT) * A.PRICE) AS REVENUE, A.UPDATE_DATE FROM COURSES, SALES A, SALES B WHERE COURSES.ID = A.ID AND A.ID = B.ID AND A.UPDATE_DATE = DATE(B.UPDATE_DATE, '+1 day') AND A.UPDATE_DATE = (?) GROUP BY A.ID, A.UPDATE_DATE ORDER BY REVENUE DESC"
+	cur = g.db.execute(sql, [date])
+	courses = [dict(ID=row[0], TITLE=row[1], PRICE=row[2], STUDENT_COUNT=row[3], REVENUE=row[4], UPDATE_DATE=row[5]) for row in cur.fetchall()]
+
+	return render_template('daily.html', DATE=date, COURSES=courses)
+
+@app.route('/course/<course_id>')
+def course_index(course_id):
+	sql = "SELECT TITLE FROM COURSES WHERE ID = (?)"
+	cur = g.db.execute(sql, [course_id])
+	title = cur.fetchall()[0][0]
+
+	sql = "SELECT STUDENT_COUNT, UPDATE_DATE FROM SALES WHERE ID = (?)"
+	cur = g.db.execute(sql, [course_id])
+	student_count = [dict(STUDENT_COUNT=row[0], UPDATE_DATE=row[1]) for row in cur.fetchall()]
+
+	sql = "SELECT A.UPDATE_DATE, A.PRICE FROM SALES A, SALES B WHERE A.ID = (?) AND A.ID = B.ID AND A.PRICE <> B.PRICE"
+	cur = g.db.execute(sql, [course_id])
+	prices = [dict(UPDATE_DATE=row[0], PRICE=row[1]) for row in cur.fetchall()]
+
+	return render_template('course.html', ID=course_id, TITLE=title, STUDENT_COUNT=student_count, PRICE=prices)
+
+@app.route('/update')
+def update():
+	crawling.write_file(crawling.get_all_urls_use_selenium())
+	file = open('./urls.txt')
+	URL_list = file.readlines()
+	file.close()
+
+	for i in range(len(URL_list)):
+		URL = URL_list[i]
+		print(URL)
+
+		course = list()
+		course.append(URL)
+		html = crawling.get_html(URL)
+		course = course + crawling.parse_html(html)
+		course.append(datetime.now().strftime('%Y-%m-%d'))
+
+		sql = "INSERT OR IGNORE INTO COURSES (URL, TITLE, RELEASE_DATE) VALUES(?, ?, ?)"
+		data = (course[0], course[1], course[4])
+		g.db.execute(sql, data)
+		g.db.commit()
+
+		sql = "SELECT ID FROM COURSES WHERE URL = (?)"
+		cur = g.db.execute(sql, [URL])
+		course_id = cur.fetchall()
+
+		sql = "INSERT OR IGNORE INTO SALES (ID, UPDATE_DATE, PRICE, STUDENT_COUNT) VALUES(?, ?, ?, ?)"
+		data = (int(course_id[0][0]), course[4], course[3], course[2])
+		g.db.execute(sql, data)
+		g.db.commit()
+
+		time.sleep(1)
+	
+	return redirect(url_for('show_courses'))
+
+@app.route('/delete')
+def delete():
+	sql = "DELETE FROM COURSES"
+	g.db.execute(sql)
+	g.db.commit()
+
+	sql = "DELETE FROM SALES"
+	g.db.execute(sql)
+	g.db.commit()
+	return redirect(url_for('show_courses'))
+
+@app.route('/ex_html')
+def ex_html():
+	return render_template('ex_html.html');
+
+if __name__ == '__main__':
+	# init_db()
+	# app.run(debug=False)
+	app.run(host='0.0.0.0', debug=False)
---
+++ sales_analysis/crawling.py
@@ -0,0 +1,129 @@
+#-*- coding: utf-8 -*-
+
+from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+import time
+import re
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+def write_file(urls):
+	f = open("urls.txt", 'w')
+	f.write('\n'.join(urls))
+	f.close()
+
+def get_html(url):
+	_html_ = ""
+	resp = requests.get(url)
+	if resp.status_code == 200:
+		_html = resp.text
+	return _html
+
+def parse_html(html):
+	course = list()
+	soup = BeautifulSoup(html, 'html.parser')
+	title = soup.find("div", {"id": "item-header-content"}).find("h1").text.strip()
+	students = int(soup.find("div", {"class": "students"}).text.replace(u'명','').strip())
+	price_area = soup.find("li", {"class": "course_price"})
+	discount = price_area.find("del")
+
+	if discount is None:
+		price = price_area.find("span", {"class": "woocommerce-Price-amount"})
+		
+		if price is None:
+			price = 0
+		else:
+			price = int(price.text.replace(',','').replace(u'₩','').strip())
+		
+	else:
+		price = price_area.find_all("span", {"class": "woocommerce-Price-amount"})[1]
+		price = int(price.text.replace(',','').replace(u'₩','').strip())
+
+	course = [title, students, price]
+	return course
+
+def remove_html_tags(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
+
+def get_urls_by_driver(driver):
+	urls = []
+	html = driver.page_source
+	soup = BeautifulSoup(html,'html.parser')
+
+	cource_dir_list = soup.find('div', id="course-dir-list")
+	cource_list = cource_dir_list.find('ul', id="course-list")
+	cource_list_items = cource_list.find_all('div', {'class':"item-title"})
+
+	for item in cource_list_items:
+		url_tag = item.find("a")
+		_url = url_tag["href"]
+		urls.append(_url)
+	return urls
+
+def get_current_page_number(driver):
+	html = driver.page_source
+	soup = BeautifulSoup(html,'html.parser')
+
+	cource_dir_list = soup.find('div', id="course-dir-list")
+	page_top = cource_dir_list.find('div', id="pag-top")
+	pagination_links = page_top.find('div', {'class':"pagination-links"})
+	currnet_page_number_html = pagination_links.find('span', {'class':"page-numbers current"})
+	currnet_page_number = int(remove_html_tags(str(currnet_page_number_html)))
+	return currnet_page_number
+
+def get_last_page_number(driver):
+	html = driver.page_source
+	soup = BeautifulSoup(html,'html.parser')
+
+	cource_dir_list = soup.find('div', id="course-dir-list")
+	page_top = cource_dir_list.find('div', id="pag-top")
+	pagination_links = page_top.find('div', {'class':"pagination-links"})
+	page_numbers = pagination_links.find_all('a', {'class':"page-numbers"})
+	last_page_number = int(remove_html_tags(str(page_numbers[-2])))
+	return last_page_number
+
+#Chrome('./chromedriver')
+#PhantomJS()
+def get_all_urls_use_selenium():
+	driver = webdriver.PhantomJS()
+	driver.get('https://www.inflearn.com/all-courses/')
+	next_page_number_xpath = "//div[@id='course-dir-list']/div[@id='pag-top']/div[@class='pagination-links']/a[@class='next page-numbers']"
+	current_xpath_element = ''
+	prev_xpath_element = ''
+	currnet_page_number = get_current_page_number(driver)
+	last_page_number = get_last_page_number(driver)
+	all_urls = []
+
+	while True:
+		try:
+			current_xpath_element = WebDriverWait(driver, 10) \
+			.until(EC.presence_of_element_located((By.XPATH, next_page_number_xpath)))
+		except Exception as e:
+			print e
+			print 'try again'
+
+		while True:
+			if current_xpath_element != prev_xpath_element or currnet_page_number == last_page_number:
+				prev_xpath_element = current_xpath_element
+				urls_one_page = get_urls_by_driver(driver)
+				for url in urls_one_page:
+					if url not in all_urls:
+						all_urls.append(url)
+				break;
+			else:
+				time.sleep(1)
+				if currnet_page_number == get_current_page_number(driver):
+					continue
+				else :
+					currnet_page_number = get_current_page_number(driver)
+				if currnet_page_number != last_page_number:
+					current_xpath_element = driver.find_element_by_xpath(next_page_number_xpath)
+		if currnet_page_number == last_page_number:
+			break;
+		current_xpath_element.click()
+
+	driver.quit()
+	return all_urls
